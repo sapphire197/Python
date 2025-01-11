77c97cc55e03d2f9a46d7374014367e766cf8ba3
sapphire197 <120254540+sapphire197@users.noreply.github.com>
2025-01-11 23:13:15 +0530

    Update PythonGame2c.py

--- PythonGame2c.py
+++ PythonGame2c.py
@@ -1,3 +1,12 @@
 #src/utils.py
 
 #Utility functions for rendering and input.
+
+import os
+
+def clear_console():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def display_board(grid):
+    clear_console()
+    print("\n".join(["\t".join(map(str, row)) for row in grid]))
